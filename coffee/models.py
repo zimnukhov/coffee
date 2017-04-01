b7fa7712ae5670604bfad97d1b11334a094a79e1
Dmitry Zimnukhov <zimnuhov@gmail.com>
2017-04-01 21:28:46 +0300

    fix models - add coffee bag

--- coffee/models.py
+++ coffee/models.py
@@ -2,44 +2,53 @@ from django.db import models
 
 
 class Roaster(models.Model):
-    name = models.Charfield(max_length=512)
+    name = models.CharField(max_length=512)
 
     def __str__(self):
         return self.name
 
 
 class Coffee(models.Model):
-    name = models.Charfield(max_length=512)
+    name = models.CharField(max_length=512)
     roaster = models.ForeignKey(Roaster)
-    purchased = models.DatetimeField()
-    ended = models.DatetimeField(blank=True, null=True)
-    found_descriptors = models.ManyToManyField(Descriptor)
+    descriptors = models.ManyToManyField('Descriptor', blank=True)
 
     def __str__(self):
         return self.name
 
 
+class CoffeeBag(models.Model):
+    coffee = models.ForeignKey(Coffee)
+    weight = models.IntegerField(blank=True, null=True)
+    roast_date = models.DateField(blank=True, null=True)
+    purchase_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return '{} ({})'.format(self.coffee.name, self.coffee.roaster.name)
+
+
 class BrewingMethod(models.Model):
-    name = models.Charfield(max_length=512)
+    name = models.CharField(max_length=512)
 
     def __str__(self):
         return self.name
 
 
 class Brew(models.Model):
-    datetime = models.DatetimeField()
+    datetime = models.DateTimeField()
     coffee = models.ForeignKey(Coffee)
     grinder_setting = models.IntegerField()
     method = models.ForeignKey(BrewingMethod)
     temperature = models.IntegerField(blank=True, null=True)
-    found_descriptors = models.ManyToManyField(Descriptor)
+    found_descriptors = models.ManyToManyField('Descriptor')
 
     def __str__(self):
         return self.datetime.strftime('%Y-%m-%d %H:%M')
 
 
 class Descriptor(models.Model):
-    name = models.Charfield(max_length=512)
+    name = models.CharField(max_length=512)
 
     def __str__(self):
         return self.name
