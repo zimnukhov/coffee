3a7c6d7f2816f4845462b121654578e5150d6506
Dmitry Zimnukhov <zimnuhov@gmail.com>
2017-04-02 12:27:42 +0300

    order brews

--- coffee/views.py
+++ coffee/views.py
@@ -10,7 +10,7 @@ def index(request):
 
     brews = Brew.objects.select_related('coffee_bag', 'method').filter(
         datetime__gt=datetime.datetime.now() - datetime.timedelta(days=7)
-    )
+    ).order_by('-datetime')
 
     return render(request, 'coffee/index.html', {
         'bags': bags,
